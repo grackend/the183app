500db8d2e899b46f77f211819dd2cd91e0a5baad
Billy Wolfington <man_of_magik903@yahoo.com>
2014-09-06 13:21:51 -0400

    Webview for syllabus tab started

--- 183App/183App/FirstViewController.swift
+++ 183App/183App/FirstViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-                            
+    
+    @IBOutlet weak var webView: UIWebView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let syllabusURLString = "https://google.com",
+            syllabusURL = NSURL(string: syllabusURLString),
+            syllabusRequest = NSURLRequest(URL: syllabusURL)
+        webView!.loadRequest(syllabusRequest)
     }
 
     override func didReceiveMemoryWarning() {
